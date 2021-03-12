ee0ce590f44222e5c6eb0ebe52eb9ac186f3deb4
ramoh <mohantyrajesh1984@gmail.com>
2021-03-12 23:38:02 +0530

    added settings module

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings
 
 
 class AlienInvasion:
@@ -8,11 +9,12 @@ class AlienInvasion:
     def __init__(self):
         """Initialise the game, and create game resource"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion - by rajesh ")
         # Set the backgroud color
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         # Run the event loop
@@ -24,7 +26,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             # Redraw the screen during each pass through the loop
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
